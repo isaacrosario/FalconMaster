ae9a6959c2633489dceebace03d7dfda95f3f5ae
Isaac <isaacrosariomarrero@gmail.com>
2018-04-13 09:48:51 -0400

    Separacion de html y js

---
+++ js/efectos.js
@@ -0,0 +1,14 @@
+
+				$(function(){
+
+
+					$("nav").slideUp(2000);
+					$("nav").slideDown(2000);
+					$("nav").hide().fadeOut(2000);
+					$("nav").hide().fadeIn(2000);
+					
+					
+					
+				});
+				
+			
