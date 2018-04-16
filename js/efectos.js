b141d6c619847211c1fa5bbb0aad0b3a5310cfe5
isaac <isaacrosariomarrero@gmail.com>
2018-04-16 09:10:05 -0400

    Gracias

--- js/efectos.js
+++ js/efectos.js
@@ -4,10 +4,10 @@
     				
 					$("#slideshow").slideUp(2000);
 					$("#slideshow").slideDown(2000);
-					
-					
+										
 					$("#logotipo").hide().fadeIn(12000);
 					$("nav").hide().fadeIn(12000);
+					
 					
 					
 					
